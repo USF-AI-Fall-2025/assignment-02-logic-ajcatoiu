b05f8d4cd4a4e727c76c623f0b3f10047a199977
andrewjcatoiu <andrewjcatoiu26@gmail.com>
2025-09-11 00:15:41 -0700

    constructor + baseline()

--- DataInvestigator.py
+++ DataInvestigator.py
@@ -1,11 +1,30 @@
+import pandas as pd
+import numpy as np
+
 class DataInvestigator:
 
-    def baseline(col):
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+        self.col_nums = {}
+        key = 0
+        for col in df.columns:
+            self.col_nums[key] = col
+            key += 1
+
+    def baseline(self, col: int):
+        if col not in self.col_nums:
+            return None
+        col_label = self.col_nums[col]
+        col_values = self.df[col_label]
+        most_freq_value = col_values.value_counts().first_valid_index()
+        return most_freq_value
+
+    def corr(self, col1: int, col2: int):
         pass
 
-    def corr(col1, col2):
+    def zeroR(self, col: int):
         pass
 
-    def zeroR(col):
-        pass
-
+df = pd.read_csv('gallstone.csv')
+di = DataInvestigator(df)
+print(di.baseline(80))
